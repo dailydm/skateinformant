4c4756779a02ad6f72b2e51bdc27feeb6f3b49a7
David <dailydm1227@gmail.com>
2019-02-27 18:17:00 -0700

    navigation working for android and ios

--- App/Containers/MainScreen.js
+++ App/Containers/MainScreen.js
@@ -5,19 +5,6 @@ import { Text, View } from 'react-native';
 // import styles from './src/themes/ApplicationStyles';
 
 export default class MainScreen extends React.Component {
-  componentDidMount() {
-    // Acrcloud.initACRCloud(
-    //   {
-    //     host: 'identify-us-west-2.acrcloud.com',
-    //     accessKey: 'e0d894343af60d9072c00a7da7488e09',
-    //     accessSecret: 'NRCM0LZhBM9P9BRpDrCBsLOOGDam6RsDRDq7wMIK',
-    //   },
-    //   error => {
-    //     console.log(`Error: ${error}`);
-    //   }
-    // );
-  }
-
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
